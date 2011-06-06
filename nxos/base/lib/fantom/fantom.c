f4c9af5ada90fcee734dea0891d329fef6859573
Tat-Chee Wan (USM) <tcwan@cs.usm.my>
2011-06-06 11:07:51 +0800

    updated gdb message header definition to use armdebug definition

--- nxos/base/lib/fantom/fantom.c
+++ nxos/base/lib/fantom/fantom.c
@@ -68,7 +68,7 @@ bool fantom_filter_packet(U8 **msgPtr, U32 *lenPtr, bool isBTComms)
 	/* WARNING: This assumes that any message matching the GDB_PROTO_MSGTYPE header is a GDB message packet.
 	 * It will break any user data transmission containing such a packet starting byte in its data stream.
 	 */
-	case GDB_PROTO_MSGTYPE:
+	case NXT_GDBMSG_TELEGRAMTYPE:
 		nxos__handleDebug(*msgPtr, (isBTComms ? COMM_BT : COMM_USB), *lenPtr);
 		/* Returned status ignored, since we don't want it to propagate up to caller */
                 status = TRUE;
