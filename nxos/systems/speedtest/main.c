a29d2d1ccb44bc2ae3b973b049e6559f2caf1496
Tat-Chee Wan (USM) <tcwan@cs.usm.my>
2012-08-02 12:22:38 +0800

    revised speedtest for optimized execution without display output and included benchmark information

--- nxos/systems/speedtest/main.c
+++ nxos/systems/speedtest/main.c
@@ -6,6 +6,8 @@
 #include "base/drivers/sensors.h"
 #include "base/drivers/radar.h"
 
+#define DISPLAY_OUT
+
 S32 A = 0;
 
 static U32 prng_get(void) {
@@ -15,20 +17,35 @@ static U32 prng_get(void) {
 }
 
 void main(void) {
-  U32 start = nx_systick_get_ms();
-  U32 end = start + 60000;
+  U32 start;
+  U32 end;
   U32 loopcount = 1;
+
+#ifndef DISPLAY_OUT
+  nx_display_cursor_set_pos(0,0);
+  nx_display_string("Start...");
+  nx_display_end_line();
+#endif
+
+  start = nx_systick_get_ms();
+  end = start + 60000;
 
   /* Initialize sensors */
   nx_sensors_analog_enable(2);
   nx_radar_init(3);
 
   do {
+#ifdef DISPLAY_OUT
+    /* Note: The Sensor reading process is synced to the NXT<->AVR read interval
+     *       The loopcount will be very low since each sensor read cycle takes 1 ms.
+     */
     U32 light = nx_sensors_analog_get(2);
     U32 us = nx_radar_read_distance(3, 0);
     U32 angle = nx_motors_get_tach_count(1) % 360;
+#endif
     U32 rn = (prng_get() % 100) + 1;
 
+#ifdef DISPLAY_OUT
     nx_display_cursor_set_pos(0,0);
 
     nx_display_uint(angle);
@@ -37,7 +54,7 @@ void main(void) {
     nx_display_uint((light + us + angle)*100/rn);
     nx_display_string("     ");
     nx_display_end_line();
-
+#endif
     nx_motors_rotate(1, rn);
     nx_motors_rotate(2, rn);
 
@@ -46,18 +63,25 @@ void main(void) {
     else if (rn < 50)
       A = MAX(A-1, -100);
 
+#ifdef DISPLAY_OUT
     if (A>=0)
       nx_display_uint(A);
     else
       nx_display_uint(-A);
     nx_display_string("     ");
     nx_display_end_line();
+#endif
     nx_motors_rotate(0, A);
+#ifdef DISPLAY_OUT
     nx_display_uint(loopcount);
     nx_display_string("     ");
+#endif
     loopcount++;
   } while(nx_systick_get_ms() < end);
 
+#ifndef DISPLAY_OUT
+  nx_display_uint(loopcount);
+#endif
   nx_display_end_line();
   nx_display_string("Done!");
   nx_motors_stop(0, TRUE);
